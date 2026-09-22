0683a5e6618a7613cac3779d1080018eaa241d64
eliaskemppi <elias.kemppi@gmail.com>
2026-09-22 11:53:12 +0300

    adding real frontend origin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[frontend_origin],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
